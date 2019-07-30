6de885e6c615c3bf7b48eba51adc0808836ac06e
HitmanJoshua <53341657+HitmanJoshua@users.noreply.github.com>
2019-07-30 05:40:43 -0700

    Add files via upload

---
+++ src/components/BookDetails.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const BookDetails = props => {
+    // console.log(props.selectedBook);
+    if(!props.selectedBook){
+        return (
+            <div>
+                Please Select a book
+            </div>
+        );
+    }
+    return (
+        <div>
+            Book Details
+            <p>
+                Title: {props.selectedBook.title}<br/>
+                Author: {props.selectedBook.author}
+            </p>
+        </div>
+    );
+};
+
+const mapStateToProps=state=>({selectedBook: state.selectedBook})
+
+export default connect(mapStateToProps)(BookDetails);
